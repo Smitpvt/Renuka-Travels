ea69a17fc99be0470dd6ce6fa57926d0d9ed0854
Smitpvt <technewssmit@gmail.com>
2026-06-28 13:56:40 +0530

    changes

--- client/src/constants/contact.js
+++ client/src/constants/contact.js
@@ -4,6 +4,6 @@ export const OFFICE_EMAIL = "renukatravels9@gmail.com";
 export const OFFICE_ADDRESS = "Labour Society, Hanuman Nagar, Vadar Pada Road No. 1, Akurli Road, Kandivali East, Mumbai - 400101";
 export const GOOGLE_MAPS_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241143.7954533247!2d72.86370769999999!3d19.2008008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b7fed1521a1b%3A0xbb50cc41cc09ebe!2sRenuka%20Travels!5e0!3m2!1sen!2sin!4v1782228505712!5m2!1sen!2sin";
 
-export const DEFAULT_WHATSAPP_MESSAGE = `Hello Renuka Travels 👋
+export const DEFAULT_WHATSAPP_MESSAGE = `Hello Renuka Travels 
 
 I would like to know more about your travel packages.`;
